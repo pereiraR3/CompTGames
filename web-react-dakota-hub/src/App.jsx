f446bcd88ee21664d7b2828ce1904cbb0e5a87b3
pereira3r <anthony_rodriguespereira@outlook.com>
2024-09-11 18:06:36 -0400

    Init React

---
+++ web-react-dakota-hub/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import LoginPage from './pages/LoginPage'
+
+function App() {
+
+  return (
+    <>
+      < LoginPage/>
+    </>
+  )
+}
+
+export default App
